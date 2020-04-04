eb73bff6a39a5ae3e4e7277a874af52de6dfa97e
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-04-04 17:53:26 +0200

    sicherung

--- newpax.lua
+++ newpax.lua
@@ -42,7 +42,56 @@ local strARRAY_END= "]"
 local strARRAY_SEP = " " 
 local strRECT_SEP = " "
 
-local constDEST_XYZ = "XYZ"
+
+local constCMD_ANNOT   = "annot"
+local constCMD_BASEURL = "baseurl" -- unused
+local constCMD_DEST    = "dest"
+local constCMD_FILE    = "file"
+local constCMD_INFO    = "info"   -- unused
+local constCMD_PAGENUM = "pagenum"
+local constCMD_PAGE    = "page"
+local constCMD_PAX     = "pax"
+
+-- cmd file
+local constKEY_SIZE    = "Size"
+local constKEY_DATE    = "Date"
+
+-- cmd annot attributes
+local constKEY_C      = "C"
+local constKEY_BORDER = "Border"
+local constKEY_BS     = "BS"
+local constKEY_H      = "H"
+
+-- cmd annot/link/URI
+local constKEY_URI = "URI"
+local constKEY_IS_MAP = "IsMap" -- handle!
+
+
+-- cmd annot/link/GoToR
+local constKEY_FILE = "File"
+local constKEY_DEST_NAME = "DestName" -- handle??
+local constKEY_DEST_PAGE = "DestPage" --ok 
+local constKEY_DEST_VIEW = "DestView" --ok
+
+-- cmd annot/link/GoTo
+local constKEY_DEST_RECT  = "DestRect" 
+local constKEY_DEST_X     = "DestX"
+local constKEY_DEST_Y     = "DestY"
+local constKEY_DEST_ZOOM  = "DestZoom"
+local constKEY_DEST_LABEL = "DestLabel" --ok
+
+-- cmd annot/link/Named
+local constKEY_NAME = "Name"
+
+-- destination views
+local constDEST_XYZ = "XYZ"   
+local constDEST_FIT    = "Fit"
+local constDEST_FITH   = "FitH"
+local constDEST_FITV   = "FitV"
+local constDEST_FITR   = "FitR"
+local constDEST_FITB   = "FitB"
+local constDEST_FITBH  = "FitBH"
+local constDEST_FITBV  = "FitBV"
 
 
 -- get/build data
@@ -57,7 +106,7 @@ local function getpagesdata (pdfedoc)
   return pagereftonum, pagecount
 end
 
--- builds a table "destination name -> obj reference (pdfedict) from the Names tree
+-- builds a table destination name -> obj reference (pdfedict) from the Names tree
 
 local function processnamesarray (pdfearray,targettable)
   for i=1,#pdfearray do
@@ -67,7 +116,6 @@ local function processnamesarray (pdfearray,targettable)
     else
       tvalue = value
       targettable[tkey]=tvalue
-     -- print("ZZZZ",table.serialize(targettable))
     end 
   end
 end
@@ -122,11 +170,11 @@ local function outputENTRY_file (file, pdfedoc)
   local date        = GETINFO(pdfedoc).CreationDate
   -- file
   local a = strENTRY_BEG
-  a = a .. strCMD_BEG .. "file" .. strCMD_END 
+  a = a .. strCMD_BEG .. constCMD_FILE .. strCMD_END 
   a = a .. strARG_BEG .. strLIT_STR_BEG .. file .. strLIT_STR_END ..  strARG_END
   a = a .. strKVS_BEG 
-   a = a .. strKV_BEG .. "Size" .. strVALUE_BEG .. bytes .. strVALUE_END .. strKV_END
-   a = a .. strKV_BEG .. "Date" .. strVALUE_BEG .. date .. strVALUE_END.. strKV_END
+   a = a .. strKV_BEG .. constKEY_SIZE .. strVALUE_BEG .. bytes .. strVALUE_END .. strKV_END
+   a = a .. strKV_BEG .. constKEY_DATE .. strVALUE_BEG .. date .. strVALUE_END.. strKV_END
   a = a .. strKVS_END 
   a = a .. strENTRY_END 
   return a
@@ -134,7 +182,7 @@ end
 
 local function outputENTRY_pagenum (pages)
   local a = strENTRY_BEG 
-  a = a .. strCMD_BEG .. "pagenum" .. strCMD_END 
+  a = a .. strCMD_BEG .. constCMD_PAGENUM .. strCMD_END 
   a = a .. strARG_BEG .. pages .. strARG_END 
   a = a .. strENTRY_END
   return a
@@ -149,7 +197,7 @@ local function outputENTRY_page (pdfedoc,page) -- page=integer
   local a=""
   if mediabox then
     a = strENTRY_BEG 
-    a = a .. strCMD_BEG .. "page" .. strCMD_END 
+    a = a .. strCMD_BEG .. constCMD_PAGE .. strCMD_END 
     a = a .. strARG_BEG .. page .. strARG_END 
     a = a .. strARG_BEG
       a = a .. mediabox[1]
@@ -166,7 +214,7 @@ end
 
 local function outputCMD_annot (pdfedict,page,type)
   local rectangle = ARRAYTOTABLE(GETARRAY(pdfedict,"Rect"))
-  local a = strCMD_BEG .. "annot" .. strCMD_END 
+  local a = strCMD_BEG .. constCMD_ANNOT .. strCMD_END 
       a = a .. strARG_BEG .. page .. strARG_END 
       a = a .. strARG_BEG .. GETNAME(pdfedict,"Subtype") .. strARG_END
       a = a .. strARG_BEG
@@ -181,11 +229,11 @@ end
 
 
 local function outputKV_color (pdfedict)
-  local color = GETARRAY(pdfedict,"C")
+  local color = GETARRAY(pdfedict,constKEY_C)
   local a =""
   if color then
     local colortable = ARRAYTOTABLE(color)
-    a = strKV_BEG .. "C" .. strVALUE_BEG .. strARRAY_BEG
+    a = strKV_BEG .. constKEY_C .. strVALUE_BEG .. strARRAY_BEG
       for i=1,#colortable do
         a = a.. colortable[i][2] .. strARRAY_SEP
       end
@@ -205,11 +253,11 @@ end
 
 -- XXX handle fourth argument
 local function outputKV_Border (pdfedict)
-  local border = GETARRAY(pdfedict,"Border")
+  local border = GETARRAY(pdfedict,constKEY_BORDER)
   local a =""
   if border then 
     local bordertable = ARRAYTOTABLE(border)
-    a = strKV_BEG .. "Border" .. strVALUE_BEG .. strARRAY_BEG
+    a = strKV_BEG .. constKEY_BORDER .. strVALUE_BEG .. strARRAY_BEG
     for i=1,3 do
       a = a .. bordertable[i][2] .. strARRAY_SEP
     end
@@ -220,11 +268,11 @@ local function outputKV_Border (pdfedict)
 end 
 
 local function outputKV_BS (pdfedict)
-  local bsstyle = GETDICTIONARY(pdfedict,"BS")
+  local bsstyle = GETDICTIONARY(pdfedict,constKEY_BS)
   local a =""
   if bsstyle then 
     local bsstyledict = DICTIONARYTOTABLE(bsstyle)
-    a = strKV_BEG .. "BS" .. strVALUE_BEG ..strDICT_BEG
+    a = strKV_BEG .. constKEY_BS .. strVALUE_BEG ..strDICT_BEG
     for k,v in pairs (bsstyledict) do
       a = a .. strNAME.. k 
       if v[1]== 5 then
@@ -240,8 +288,8 @@ end
 
 
 local function outputKV_uri (pdfedict)
-  local type, value, hex = GETFROMDICTIONARY(pdfedict,"URI")
-  local a= strKV_BEG .. "URI" .. strVALUE_BEG 
+  local type, value, hex = GETFROMDICTIONARY(pdfedict,constKEY_URI)
+  local a= strKV_BEG .. constKEY_URI .. strVALUE_BEG 
   if hex then 
     a = a .. strHEX_STR_BEG .. value .. strHEX_STR_END 
   else
@@ -253,7 +301,7 @@ end
 
 local function outputKV_N (pdfedict)
   local name = GETNAME(pdfedict,"N")
-  local a= strKV_BEG .. "Name" .. strVALUE_BEG .. name .. strVALUE_END .. strKV_END
+  local a= strKV_BEG .. ConstKEY_NAME .. strVALUE_BEG .. name .. strVALUE_END .. strKV_END
   return a
 end 
 
@@ -261,28 +309,28 @@ local function outputKV_gotor (pdfedict) -- action dictionary
   local type, value, hex = GETFROMDICTIONARY(pdfedict,"F")
   local type, pagenum    = GETFROMARRAY(GETARRAY (pdfedict,"D"),1)
   local type, fittype    = GETFROMARRAY(GETARRAY (pdfedict,"D"),2)  
-  local a =  strKV_BEG .."File" .. strVALUE_BEG
+  local a =  strKV_BEG .. constKEY_FILE .. strVALUE_BEG
   if hex then
     a = a .. strHEX_STR_BEG .. value .. strHEX_STR_end
   else
     a = a .. strLIT_STR_BEG .. value .. strLIT_STR_END
   end
   a = a .. strVALUE_END .. strKV_END 
-  a = a .. strKV_BEG .. "DestPage" .. strVALUE_BEG .. pagenum .. strVALUE_END .. strKV_END
-  a = a .. strKV_BEG .. "DestView" .. strVALUE_BEG .. strNAME .. fittype .. strVALUE_END .. strKV_END
+  a = a .. strKV_BEG .. constKEY_DEST_PAGE .. strVALUE_BEG .. pagenum .. strVALUE_END .. strKV_END
+  a = a .. strKV_BEG .. constKEY_DEST_VIEW .. strVALUE_BEG .. strNAME .. fittype .. strVALUE_END .. strKV_END
   return a    
 end
 
 
 local function outputKV_goto (count)
-  local a = strKV_BEG .. "DestLabel" .. strVALUE_BEG .. count .. strVALUE_END .. strKV_END
+  local a = strKV_BEG .. constKEY_DEST_LABEL .. strVALUE_BEG .. count .. strVALUE_END .. strKV_END
   return a
 end 
 
 local function outputENTRY_dest (destcount,name,pagereftonum,destnamestoref)
  local pagenum, data = getdestdata(name,pagereftonum,destnamestoref)
  local a = strENTRY_BEG
- a = a .. strCMD_BEG .. "dest" .. strCMD_END
+ a = a .. strCMD_BEG .. constCMD_DEST .. strCMD_END
  a = a .. strARG_BEG .. pagenum .. strARG_END 
  a = a .. strARG_BEG .. destcount .. strARG_END
  -- name
@@ -290,34 +338,34 @@ local function outputENTRY_dest (destcount,name,pagereftonum,destnamestoref)
  a = a .. strKVS_BEG
  if data[2][2] == CONSTDEST_XYZ then   
    if data[3][2] then 
-    a = a .. strKV_BEG .. "DestX" .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_X .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
    end
    if data[4][2] then 
-    a = a .. strKV_BEG .. "DestY" .. strVALUE_BEG .. data[4][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_Y .. strVALUE_BEG .. data[4][2] .. strVALUE_END .. strKV_END
    end
    if data[5][2] then 
-    a = a .. strKV_BEG .. "DestZoom" .. strVALUE_BEG .. data[5][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_ZOOM .. strVALUE_BEG .. data[5][2] .. strVALUE_END .. strKV_END
    end 
- elseif data[2][2] == "Fit"  then -- nothing to do
- elseif data[2][2] == "FitB" then -- nothing to do
- elseif data[2][2] == "FitH" then
+ elseif data[2][2] == constDEST_FIT  then -- nothing to do
+ elseif data[2][2] == constDEST_FITB then -- nothing to do
+ elseif data[2][2] == constDEST_FITH then
    if data[3][2] then 
-    a = a .. strKV_BEG .. "DestY" .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_Y .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
    end
- elseif data[2][2] == "FitBH" then
+ elseif data[2][2] == constDEST_FITBH then
    if data[3][2] then 
-    a = a .. strKV_BEG .. "DestY" .. strVALUE_BEG  .. data[3][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_Y .. strVALUE_BEG  .. data[3][2] .. strVALUE_END .. strKV_END
    end   
- elseif data[2][2] == "FitV" then
+ elseif data[2][2] == constDEST_FITV then
    if data[3][2] then 
-    a = a .. strKV_BEG .. "DestX" .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_X .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
    end
- elseif data[2][2] == "FitBV" then
+ elseif data[2][2] == constDEST_FITBV then
    if data[3][2] then 
-    a = a .. strKV_BEG .. "DestX" .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
+    a = a .. strKV_BEG .. constKEY_DEST_X .. strVALUE_BEG .. data[3][2] .. strVALUE_END .. strKV_END
    end   
- elseif data[2][2] == "FitR" and data[6] then   
-   a = a ..  strKV_BEG .. "DestRect" .. strVALUE_BEG  
+ elseif data[2][2] == constDEST_FITR and data[6] then   
+   a = a ..  strKV_BEG .. constKEY_DEST_RECT .. strVALUE_BEG  
    a = a .. data[3][2] .. strRECT_SEP
    a = a .. data[4][2] .. strRECT_SEP 
    a = a .. data[5][2] .. strRECT_SEP 
@@ -365,10 +413,10 @@ local function __writepax (ext,file)
           WRITE (outputCMD_annot(annot,i,annotactiontype)) 
           WRITE (strKVS_BEG) -- begin KVS data 
           WRITE ( outputKV_color(annot) )
-          WRITE ( outputKV_key(annot,"H") )
+          WRITE ( outputKV_key(annot,constKEY_H) )
           WRITE ( outputKV_Border (annot) )
           WRITE ( outputKV_BS (annot) )
-          if annotactiontype =="URI" then 
+          if annotactiontype == constKEY_URI then 
             WRITE ( outputKV_uri(annotaction) )
           elseif annotactiontype =="GoTo" then
             destcountVAR=destcountVAR + 1
