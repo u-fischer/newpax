c4a32e4c7d10754910cbe1314312dfac80465536
Ulrike Fischer <fischer@troubleshooting-tex.de>
2020-04-05 23:52:43 +0200

    sicherung

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# newpax
+
+![Version: 0.10](https://img.shields.io/badge/current_version-0.10-blue.svg?style=flat-square)
+![Date: 2020-04-05](https://img.shields.io/badge/date-2020-04-05-blue.svg?style=flat-square)
+[![License: LPPL1.3c ](https://img.shields.io/badge/license-LPPL1.3c-blue.svg?style=flat-square)](https://ctan.org/license/lppl1.3c)
+
+The package is based on the pax package from Heiko Oberdiek. 
+  If offers a lua-based alternative to the java based pax.jar to extract the annotations from a PDF. The resulting file
+  can then be used together with pax.sty. It also offers an extended style which works with all three major engines.
+
+
+##  Structure
+
+- newpax.lua and newpax.sty are in the main folder. 
+
+- The doc folder contains the documentation 
+    
+- testfiles contains tests for the l3build system. 
+      
+## Rules for contribuations
+
+Comments, feedback, examples are welcome. 
+
+Use the issue tracker, sent me a mail or make a pull-request.
+
+## Licence
+
+The newpax package may be modified and distributed under the terms and conditions of the 
+[LaTeX Project Public License](https://www.latex-project.org/lppl/), version 1.3c or greater.
+ 
+
+
---
+++ build.lua
@@ -0,0 +1,82 @@
+-- Build script for newpax
+packageversion="0.1"
+packagedate="2020-04-05"
+
+module   = "newpax"
+ctanpkg  = "newpax"
+
+local ok, mydata = pcall(require, "ulrikefischerdata.lua")
+if not ok then
+  mydata= {email="XXX",github="XXX",name="XXX"}
+end
+
+uploadconfig = {
+  pkg     = ctanpkg,
+  version = "v"..packageversion.." "..packagedate,
+  author  = mydata.name,
+  license = "lppl1.3c",
+  summary = "Experimental package to extract and reinsert PDF annotations",
+  -- ctanPath = "????????",
+  repository = mydata.github .. "newpax",
+  bugtracker = mydata.github .. "newpax/issues",
+  support    = mydata.github .. "newpax/issues",
+  uploader = mydata.name,
+  email    = mydata.email,
+  update   = true ,
+  topic=    "pdf-feat",
+  note     = [[ Uploaded automatically by l3build...]],
+  description=[[The package is based on the pax package from Heiko Oberdiek. 
+  If offers a lua-based alternative to the java based pax.jar to extract the annotations from a PDF. The resulting file
+  can then be used together with pax.sty. It also offers an extended style which works with all three major engines.]],
+  announcement_file="ctan.ann"              
+}
+
+--specialformats = specialformats or {}
+--
+--if string.find(status.banner,"2019") then
+--  print("TL2019")
+--  TL2019bool=true
+--else 
+--  -- tl2020
+--  print("TL2020 or later")
+--
+--  specialformats["latex"] = specialformats["latex"] or 
+--   {
+--    luatex     = {binary="luahbtex",format = "lualatex"},
+--   }
+--  specialformats["latex-dev"] = specialformats["latex-dev"] or 
+--   {
+--    luatex = {binary="luahbtex",format = "lualatex-dev"}
+--   }
+--end
+
+checkengines = {"luatex","pdftex","xetex"}
+checkconfigs = {"build"}
+
+checkruns = 3
+checksuppfiles = {"newpax-input.pdf"}
+
+
+-- ctan setup
+docfiles = {"doc/*tagpdf.tex",
+textfiles= {"doc/CTANREADME.md"}
+ctanreadme= "CTANREADME.md"
+
+typesetexe = "lualatex"
+packtdszip   = false
+installfiles = {
+                "**/*.sty",
+                "**/*.lua"
+               }  
+               
+sourcefiles  = {
+                "**/*.sty",
+                "**/*.lua"
+               }
+                            
+typesetfiles = {"doc/newpax.tex"}
+typesetdemofiles = {"doc/doc-extract.tex","doc/doc-pax-test.tex","doc/doc-newpax-test.tex"}
+
+typesetruns = 4
+
+
